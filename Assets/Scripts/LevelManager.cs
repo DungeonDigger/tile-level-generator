3b585f9bd8e054520af8130ac4ed2aeb26620a85
Evan Sheffield <evan.c.sheffield@gmail.com>
2018-05-27 17:52:00 -0400

    feature: Add Dijkstra implementation for finding shortest paths

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
@@ -77,6 +78,76 @@ public class LevelManager : MonoBehaviour {
             }
         }
         level[x, y] = tile;
+    }
+
+    /// <summary>
+    /// Gets the shortest path distance between two locations in the level
+    /// using Dijkstra's algorithm.
+    /// 
+    /// Precondition is that the starting and ending location must be non-block
+    /// tiles (i.e. they must be traversable)
+    /// </summary>
+    /// <param name="startX">x value of the starting tile</param>
+    /// <param name="startY">y value of the starting tile</param>
+    /// <param name="destX">x value of the destination tile</param>
+    /// <param name="destY">y value of the destination tile</param>
+    /// <returns>Shortest path distance between the two points, or -1 if
+    /// the destination could not be reached.</returns>
+    public int GetShortestPathDistance(int startX, int startY, int destX, int destY)
+    {
+        List<string> unvisited = new List<string>();
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                if (level[i, j] != CELL_BLOCK) unvisited.Add(i + "_" + j);
+
+        Dictionary<string, int> distances = new Dictionary<string, int>();
+
+        foreach (var v in unvisited)
+        {
+            distances[v] = int.MaxValue;
+        }
+        distances[startX + "_" + startY] = 0;
+        var targetLoc = destX + "_" + destY;
+        while(unvisited.Count > 0)
+        {
+            // Find the unvisited node with the shortest distance
+            string toVisit = null;
+            foreach (var v in unvisited)
+            {
+                if(toVisit == null || distances[v] < distances[toVisit])
+                {
+                    toVisit = v;
+                }
+            }
+            unvisited.Remove(toVisit);
+
+            // We've reach our destination! Return the distance
+            if (toVisit == targetLoc)
+                return distances[toVisit];
+
+            // Visit all of the node's neighbors and update their distances
+            int x = int.Parse(toVisit.Split('_')[0]);
+            int y = int.Parse(toVisit.Split('_')[1]);
+            List<string> neighbors = new List<string>();
+            if (x - 1 > 0 && level[x - 1, y] != CELL_BLOCK)
+                neighbors.Add(x - 1 + "_" + y);
+            if (x + 1 < width && level[x + 1, y] != CELL_BLOCK)
+                neighbors.Add(x + 1 + "_" + y);
+            if (y - 1 > 0 && level[x, y - 1] != CELL_BLOCK)
+                neighbors.Add(x + "_" + (y - 1));
+            if (y + 1 < height && level[x - 1, y] != CELL_BLOCK)
+                neighbors.Add(x - 1 + "_" + y);
+
+            int altDistance = distances[toVisit] + 1;
+            foreach (var neighbor in neighbors)
+            {
+                if (altDistance < distances[neighbor])
+                    distances[neighbor] = altDistance;
+            }
+        }
+
+        // Our destination was not found
+        return -1;
     }
 
     private void InitializeEmptyLevel()
